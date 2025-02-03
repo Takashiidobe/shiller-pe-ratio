f3e2fd051142fe268bc6ae6bf113ed9ff03c8cb7
Takashiidobe <idobetakashi@gmail.com>
2025-02-03 18:46:59 -0500

    factor out into main and library

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use axum::{
+    http::{header, StatusCode, Uri},
+    response::{Html, IntoResponse, Response},
+    routing::{get, Router},
+};
+use datadog_tracing::axum::shutdown_signal;
+use datadog_tracing::axum::{OtelAxumLayer, OtelInResponseLayer};
+use rust_embed::Embed;
+use std::{net::SocketAddr, time::Duration};
+use tower_http::timeout::TimeoutLayer;
+use tracing::info;
+
+pub async fn app() {
+    let (_guard, tracer_shutdown) = datadog_tracing::init().unwrap();
+    let port: u16 = std::env::var("PORT")
+        .unwrap_or("3000".to_string())
+        .parse()
+        .unwrap();
+
+    let app = Router::new()
+        .route("/", get(index_handler))
+        .route("/index.html", get(index_handler))
+        .route("/{*wildcard}", get(static_handler))
+        .layer(OtelInResponseLayer)
+        .layer((
+            OtelAxumLayer::default(),
+            TimeoutLayer::new(Duration::from_secs(10)),
+        ))
+        .fallback_service(get(not_found));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    println!("listening on {}", addr);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+
+    tracer_shutdown.shutdown();
+}
+
+async fn index_handler() -> impl IntoResponse {
+    static_handler("/index.html".parse::<Uri>().unwrap()).await
+}
+
+async fn static_handler(uri: Uri) -> impl IntoResponse {
+    let path = uri.path().trim_start_matches('/').to_string();
+
+    StaticFile(path)
+}
+
+async fn not_found() -> Html<&'static str> {
+    Html("<h1>404</h1><p>Not Found</p>")
+}
+
+#[derive(Embed)]
+#[folder = "build/"]
+pub struct Asset;
+
+pub struct StaticFile<T>(pub T);
+
+impl<T> IntoResponse for StaticFile<T>
+where
+    T: Into<String>,
+{
+    fn into_response(self) -> Response {
+        let path = self.0.into();
+
+        match Asset::get(path.as_str()) {
+            Some(content) => {
+                let mime = mime_guess::from_path(&path).first_or_octet_stream();
+                info!("Serving {}", path.as_str());
+                ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+            }
+            None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+        }
+    }
+}
